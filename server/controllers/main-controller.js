e292b9e6ccf03650f9213111e03b4c0b6e2425a5
racha022 <rahul.chand@javra.com>
2025-04-01 21:31:54 +0545

    s

--- server/controllers/main-controller.js
+++ server/controllers/main-controller.js
@@ -40,7 +40,7 @@ cron.schedule("15 02 * * *", () => {
   activateMatch();
 });
 
-cron.schedule("15,33 10,11,12,14,16,17,18,19 * * *", () => {
+cron.schedule("15,45 10,11,12,14,15,16,17,18,19 * * *", () => {
   console.log("Match Import Start");
   importScoreCard();
 });
@@ -49,7 +49,7 @@ cron.schedule("20,40 10-19 * * *", () => {
   importFromScorecard();
 });
 
-cron.schedule("*/20 14-23 * * *", () => {
+cron.schedule("*/10 14-23 * * *", () => {
   console.log("Fantasy Points Update Start");
   updateFantasyPoints();
 });
